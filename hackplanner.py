e3be34bbb1251293bc63308983a486638bcf1f73
Manasvi <91024452+miinus-vee@users.noreply.github.com>
2023-07-07 03:39:25 +0530

    Update hackplanner.py

--- hackplanner.py
+++ hackplanner.py
@@ -88,7 +88,7 @@ create_table()
 
 root = tk.Tk()
 root.title("HackPlanner")
-root.geometry("600x500")
+root.geometry("1000x1500")
 root.config(bg="#FBE9E7")
 
 header_frame = tk.Frame(root, bg="#F48FB1")
@@ -116,13 +116,13 @@ task_date_entry = DateEntry(add_task_frame, width=12, background="#FFF", foregro
 task_date_entry.grid(row=1, column=1, padx=5, pady=5)
 
 add_button = tk.Button(main_frame, text="Add Task", command=add_task, bg="#F48FB1", fg="#FFF", font=("Arial", 16))
-add_button.pack(pady=10)
+add_button.pack(pady=5)
 
 tasks_frame = tk.Frame(main_frame, bg="#FFF")
 tasks_frame.pack()
 
 tasks_label = tk.Label(tasks_frame, text="Tasks", font=("Arial", 24), bg="#FFF")
-tasks_label.pack(pady=10)
+tasks_label.pack(pady=5)
 
 task_list = tk.Listbox(tasks_frame, width=50, font=("Arial", 14))
 task_list.pack(pady=5)
@@ -133,7 +133,7 @@ reminder_button = tk.Button(main_frame, text="Set Reminder", command=set_reminde
 reminder_button.pack(pady=10)
 
 alarm_frame = tk.Frame(main_frame, bg="#FFF")
-alarm_frame.pack(pady=10)
+alarm_frame.pack(pady=5)
 
 alarm_time_label = tk.Label(alarm_frame, text="Alarm time (HH:MM):", bg="#FFF", font=("Arial", 16))
 alarm_time_label.grid(row=0, column=0, padx=5, pady=5)
@@ -142,7 +142,7 @@ alarm_time_entry = tk.Entry(alarm_frame, width=10, font=("Arial", 16))
 alarm_time_entry.grid(row=0, column=1, padx=5, pady=5)
 
 alarm_button = tk.Button(main_frame, text="Set Alarm", command=set_alarm, bg="#F48FB1", fg="#FFF", font=("Arial", 16))
-alarm_button.pack(pady=10)
+alarm_button.pack(pady=5)
 
 footer_label = tk.Label(root, text="© 2023 HackPlanner. All rights reserved.", fg="#FFF", bg="#F48FB1", pady=10)
 footer_label.pack(fill=tk.X)
